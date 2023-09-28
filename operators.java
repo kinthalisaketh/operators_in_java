b32190a5a9cb5e003f20d7a4146a2faa0f52421c
Kinthali Saketh <103805011+kinthalisaketh@users.noreply.github.com>
2023-09-28 18:24:12 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,18 @@
+public class operators {
+    public static void main(String[] args){
+        int a = 4;
+        int b= 6+a;
+        int c= 6-a;
+        int d=6*a; 
+        int e =6 % a;
+        b+=5;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(b);
+        System.out.println(a>b);
+        System.out.println(2&3);
+    }
+    
+}
